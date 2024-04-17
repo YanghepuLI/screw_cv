f9b98bb7806ded009a5994607c8616d318448cce
YanghepuLI <88429495+YanghepuLI@users.noreply.github.com>
2024-04-18 01:08:38 +0900

    Add files via upload

---
+++ env.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+pip install pandas
+pip install numpy
+pip install seaborn
+pip install opencv-python
+pip install matplotlib
+pip install plotly
+pip install torch
+pip install torchvision
+pip install sklearn
+pip install PIL
